7ec233074ee9db281acaea8a6ef3dd12a6e24da3
luciovw <luciovw@gmail.com>
2020-08-24 16:32:29 +0000

    Project4 Part2 Revised4

--- main.cpp
+++ main.cpp
@@ -408,9 +408,11 @@ IntType& IntType::divide( int rhs )
     if (rhs == 0)
     {
         std::cout << "error: integer division by zero is an error and will crash the program!" << std::endl;
-        return *this;
     }
+    else
+    {
     *value /= rhs;
+    }
     return *this;
 }
 //------------------------------------------------------
